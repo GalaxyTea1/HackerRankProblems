ce1676a03fe7afa3d2a10047638eaaab32bca45c
trongdv <trongdv031199@gmail.com>
2025-03-30 13:38:21 +0700

    LibraryFine.js

--- LibraryFine.js
+++ LibraryFine.js
@@ -36,8 +36,12 @@ function readLine() {
  */
 
 function libraryFine(d1, m1, y1, d2, m2, y2) {
-    // Write your code here
-
+    if (y1 > y2) return 10000;
+    if (y1 === y2) {
+        if (m1 > m2) return 500 * (m1 - m2);
+        if (m1 === m2 && d1 > d2) return 15 * (d1 - d2);
+    }
+    return 0;
 }
 
 function main() {
