131f04ff928db9f7087ae4033b572f4f335aca84
trongdv <trongdv031199@gmail.com>
2025-05-22 21:21:23 +0700

    EqualizeTheArray.js

--- EqualizeTheArray.js
+++ EqualizeTheArray.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,20 +30,36 @@ function readLine() {
  */
 
 function equalizeArray(arr) {
-    // Write your code here
+  const frequency = {};
 
+  for (let num of arr) {
+    frequency[num] = (frequency[num] || 0) + 1;
+  }
+
+  let maxFrequency = 0;
+
+  for (let count of Object.values(frequency)) {
+    if (count > maxFrequency) {
+      maxFrequency = count;
+    }
+  }
+
+  return arr.length - maxFrequency;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const result = equalizeArray(arr);
+  const result = equalizeArray(arr);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
